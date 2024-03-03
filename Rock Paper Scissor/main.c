0a2501861914b6f0ee64f5e5b6420181270bd0d5
Ansh Dhunna <Adhunna1172@conestogac.on.ca>
2024-03-03 14:20:58 -0500

    Implementation

---
+++ Rock Paper Scissor/main.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string.h>
+
+const char* Result(const char player1[], const char player2[]) {
+
+	if ((strcmp(player1, "rock") == 0 && strcmp(player2, "scissor") == 0) ||
+		(strcmp(player1, "scissor") == 0 && strcmp(player2, "paper") == 0) ||
+		(strcmp(player1, "paper") == 0 && strcmp(player2, "rock") == 0)) {
+		return "Player1";
+	}
+
+	else if ((strcmp(player1, player2) == 0)) {
+		return "Draw";
+	}
+
+	else {
+		return "Player2";
+	}
+}
+
+int main(void){
+
+	printf(" Lets Play Rock Paper Scissor \n\n");
+
+	return 0;
+
+}
